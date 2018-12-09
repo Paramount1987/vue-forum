5bef4850132e3b8f6590d4636e9b531b0baf54bd
Artem Petrov <Paramount7891@gmail.com>
2018-12-09 15:03:42 +0300

    Using Environment Variables in the Vue.js Webpack Template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: '"AIzaSyBVxh41PpQcmDOCzbZi9sSGqjUO4nJ_N-M"',
+  FIREBASE_AUTH_DOMAIN: '"vue-school-forum-224bd.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://vue-school-forum-224bd.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"vue-school-forum-224bd"',
+  FIREBASE_STORAGE_BUCKET: '"vue-school-forum-224bd.appspot.com"',
+  FIREBASE_MESSAGING_ID: '"167837826982"'
 }
--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyBVxh41PpQcmDOCzbZi9sSGqjUO4nJ_N-M',
-  authDomain: 'vue-school-forum-224bd.firebaseapp.com',
-  databaseURL: 'https://vue-school-forum-224bd.firebaseio.com',
-  projectId: 'vue-school-forum-224bd',
-  storageBucket: 'vue-school-forum-224bd.appspot.com',
-  messagingSenderId: '167837826982'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
 }
 firebase.initializeApp(config)
 
