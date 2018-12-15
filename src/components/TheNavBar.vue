6ce05d577ce0bb894ada7c7d20115ca18e1f6a07
Artem Petrov <Paramount7891@gmail.com>
2018-12-15 22:11:17 +0300

    close dropdown on click outside

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -18,7 +18,7 @@
     <!-- use .navbar-open to open nav -->
     <nav class="navbar">
       <ul v-if="user">
-        <li class="navbar-user">
+        <li class="navbar-user" v-click-outside="closeUserDropdown">
           <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="user.avatar" alt="">
             <span>
@@ -56,8 +56,13 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import clickOutside from '@/directives/click-outside'
 
 export default {
+  directives: {
+    clickOutside
+  },
+
   data () {
     return {
       userDropdownOpen: false
@@ -67,6 +72,15 @@ export default {
     ...mapGetters({
       'user': 'auth/authUser'
     })
+  },
+
+  methods: {
+    closeUserDropdown () {
+      this.userDropdownOpen = false
+    },
+    closeMobileNavbar () {
+      this.mobileNavOpen = false
+    }
   }
 }
 </script>
